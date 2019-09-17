34f90f2f8bcc4fc26c4746ed6d155fbdbd1eb086
KamilaGor <kamilagorniak89@gmail.com>
2019-09-17 18:31:44 +0200

    1

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
@@ -30,7 +31,8 @@ public class TaskController {
 	}
 
 	@RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
-	public void deleteTask(@RequestParam Long taskId) { service.deleteTask(taskId);
+	public void deleteTask(@RequestParam Long taskId) {
+		service.deleteTask(taskId);
 	}
 
 	@RequestMapping(method = RequestMethod.PUT, value = "updateTask")
